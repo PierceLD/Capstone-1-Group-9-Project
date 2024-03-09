446e48bab8a6cfe8fa3a35a4059634616297ae96
cas72p <cas72p@umsystem.edu>
2024-03-08 22:14:32 -0600

    Added music
    added music.py and the ability to play mp3's

---
+++ music.py
@@ -0,0 +1,27 @@
+# Assuming this is in music.py
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtCore import QUrl
+import time
+
+class AudioPlayer:
+    def __init__(self):
+        self.player = QMediaPlayer()
+        self.audioOutput = QAudioOutput()
+        self.player.setAudioOutput(self.audioOutput)
+
+    def playMusic(self, filePath):
+        if self.player.source() != QUrl.fromLocalFile(filePath):
+            self.player.setSource(QUrl.fromLocalFile(filePath))
+        if self.player.playbackState() != QMediaPlayer.PlaybackState.PlayingState:
+            self.player.play()
+
+    def stopMusic(self):
+        self.player.stop()
+
+    def changeAndPlayMusic(self, filePath):
+        self.stopMusic()  
+        time.sleep(.05)
+        self.playMusic(filePath)
+    def setVolume(self, volume):
+        volume = max(0.0, min(1.0, volume))
+        self.audioOutput.setVolume(volume)
