ccd15f32b3ea7db76b39b36d328384b8a3ba3466
Khai Thieu <khaithieu3@gmail.com>
2024-04-02 18:52:31 -0500

    Displays questions and answers in the selected study set

--- Study.py
+++ Study.py
@@ -12,5 +12,17 @@ class StudyScreen(QWidget):
       self.studySetName.setAlignment(Qt.AlignmentFlag.AlignCenter)
     
     def load(self, study_set_name):
-       self.studySetName.setText(study_set_name)
+      self.studySetName.setText(study_set_name)
     
+      with open("sets.json", "r") as json_file:
+        try:
+            self.selected_set = json.load(json_file)[study_set_name]
+        except:
+            print("Empty json file")
+
+      self.all_question_answer = ""
+      for item in self.selected_set:
+        self.all_question_answer += "Question: " + item['Question'] + "\n"
+        self.all_question_answer += "Answer: " + item['Answer'] + "\n\n"
+        
+      self.questionAnswer.setText(self.all_question_answer)
