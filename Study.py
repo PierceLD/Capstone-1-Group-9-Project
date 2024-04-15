42e80de9fbfea0b07d65b6d8db6963dd5694ee0e
Caleb Schoenberg <caleb.schoenberg@hotmail.com>
2024-04-14 19:43:55 -0500

    Fixed set creation autogenerate answer choices, added hide answers button to the study screen

--- Study.py
+++ Study.py
@@ -11,6 +11,7 @@ class StudyScreen(QWidget):
 
       self.studySetName.setAlignment(Qt.AlignmentFlag.AlignCenter)
       self.showAnswers.clicked.connect(self.show_answers)
+      self.answers_shown = False
     
     def load(self, study_set_name):
       self.studySetName.setText(study_set_name)
@@ -25,8 +26,16 @@ class StudyScreen(QWidget):
       
     def show_answers(self):
       self.all_question_answer = ""
-      for item in self.selected_set:
-        self.all_question_answer += item['question'] + "\n"
-        self.all_question_answer += "Answer: " + item[item['answer']] + "\n\n"
+
+      if self.answers_shown:
+        self.load(self.studySetName.text())
+        self.showAnswers.setText("Show Answers")
+      else:
+        for item in self.selected_set:
+          self.all_question_answer += item['question'] + "\n"
+          self.all_question_answer += "Answer: " + item[item['answer']] + "\n\n"
+          self.showAnswers.setText("Hide Answers")
+      
+      self.answers_shown = not self.answers_shown
         
       self.questionAnswer.setText(self.all_question_answer)
